f596063dd71fc71cfbfeb630b0b825b692a633e8
EC2 Default User <ec2-user@ip-172-31-39-55.ap-northeast-1.compute.internal>
2022-10-04 08:24:20 +0000

    [Finish]9b

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,6 @@
 //= require jquery
 //= require rails-ujs
+//= require activestorage
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,12 @@ Rails.application.routes.draw do
     resources :book_comments,only:[:create,:destroy]
     resource :favorites,only:[:create,:destroy]
   end
+
   resources :users, only: [:index,:show,:edit,:update] do
     resource :relationships,only: [:create,:destroy]
     get 'follows' => 'relationships#followings'
     get 'followers' => 'relationships#followers'
-
+    get 'search', to: 'users#search'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
